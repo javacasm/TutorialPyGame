01b8e3f8494cd01cd68c60b478889032a1722c01
Javacasm <javacasm@gmail.com>
2020-06-15 16:54:43 +0200

    update

--- codigo/tutorial/tut01_base.py
+++ codigo/tutorial/tut01_base.py
@@ -1,7 +1,31 @@
+"""
+Tutorial básico de pyGame
+
+01 - Ejemplo base: 
+* pygame.init() # Inicializa el entorno de pygame
+* Mostramos una venana de tamaño width, height 
+* Con título  display.set_caption('Título')
+* Comprueba los eventos 
+    * Responde al evento de cierre
+* display.flip() para actualizar la ventana 
+
+
+Docs: https://www.pygame.org/docs/ref/display.html
+
+display.flip: Actualiza toda la surface a pantalla
+display.update(rect): Actualiza un rectángulo  (¿sin argumentos actualiza todo?)
+
+CC by SA @javacasm
+Junio 2020
+
+"""
+
 import pygame
 
 width = 640
 height = 400
+
+pygame.init() # Inicializa el entorno de pygame
 
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Ejemplo base')
