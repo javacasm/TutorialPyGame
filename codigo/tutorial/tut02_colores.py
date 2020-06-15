2fc335d620fc52874bc2065f2c5aed8d01b0fa57
Javacasm <javacasm@gmail.com>
2020-06-15 16:54:55 +0200

    update

--- codigo/tutorial/tut02_colores.py
+++ codigo/tutorial/tut02_colores.py
@@ -1,3 +1,26 @@
+"""
+Tutorial básico de pyGame
+
+02 - Definimos colores de diferente formas:
+* Usando la clase pygame.Color:
+    * Usando nombres de colores : 'Red', 'Blue', 'Black', 'Aqua' ¿Listado?
+    * Usando intensidad de cada color ¿Alpha?
+* Usando una tupla de 3 valores para la intensidad ¿Alpha?
+
+Usamos el método fill sobre surface para rellenar la ventana de un color dado
+
+Docs: 
+* Sobre la clase Color https://www.pygame.org/docs/ref/color.html
+* Colores definidos https://github.com/pygame/pygame/blob/master/src_py/colordict.py
+
+
+
+
+CC by SA @javacasm
+Junio 2020
+
+"""
+
 import pygame
 
 width = 640
@@ -6,12 +29,13 @@ height = 400
 
 # 3 formas de definir los colores
 red = pygame.Color('Red')
-agua = pygame.Color('Aqua')
+cyan = pygame.Color('cyan')
 blue = pygame.Color(0,0,255) # ¿alpha?
 green = (0,  255, 0)
 white = (255,255,255)
 black = (0,0,0)
 
+pygame.init() # Inicializa el entorno de pygame
 
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Ejemplo colores')
