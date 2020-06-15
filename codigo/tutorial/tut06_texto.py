4b39058cfafa1b7a712320055e00751b9377af36
Javacasm <javacasm@gmail.com>
2020-06-15 16:55:16 +0200

    update

--- codigo/tutorial/tut06_texto.py
+++ codigo/tutorial/tut06_texto.py
@@ -1,3 +1,33 @@
+"""
+Tutorial básico de pyGame
+
+06 - Texto y Fonts
+
+Creación de font:
+Font de usuario
+* pygame.font.Font('fonts/myfont.ttf', size)  espera el path a un fichero con la fuente
+Fonts del sistema
+* pygame.font.SysFont('arial', size) busca una fuente en sistema con el nombre indicado
+
+Sobre un font podemos hacer
+
+* fornt.set_italic()
+* fornt.set_bold()
+* fornt.set_underline()
+
+Podemos saber el tamaño de un texto con
+font.size(text) -> (width, height)
+
+Con render generamos en una superfice
+surface = font.render(texto, antialias, color)
+
+Docs sobre fonts: https://www.pygame.org/docs/ref/font.html
+
+CC by SA @javacasm
+Junio 2020
+
+"""
+
 import pygame
 
 width = 640
@@ -6,27 +36,20 @@ height = 400
 
 # 3 formas de definir los colores
 red = pygame.Color('Red')
-agua = pygame.Color('Aqua')
+cyan = pygame.Color('cyan')
 blue = pygame.Color(0,0,255) # ¿alpha?
 green = (0,  255, 0)
 white = (255,255,255)
 black = (0,0,0)
 
-# Sobre los fonts https://www.pygame.org/docs/ref/font.html
 
-"""
-pygame.font.Font() espera el path a un fichero con la fuente
-pygame.font.Font('fonts/myfont.ttf', size)
-
-pygame.font.SysFont() busca una fuente en sistema con el nombre indicado
-pygame.font.SysFont('arial', size)
-"""
+pygame.init() # Inicializa el entorno de pygame
 
 for font in pygame.font.get_fonts():
     print(font)
 
 
-
+sysFont = pygame.sysfont.SysFont('freemono',20,bold = True)
 
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Ejemplo Texto y fonts')
@@ -38,22 +61,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False 
     
-    screen.fill(green) # copia screen en la pantalla
+    screen.fill(cyan) # copia screen en la pantalla
 
-"""
-Sobre un font podemos hacer
+    textSurface = sysFont.render('Una prueba', True, black)
+    rect = textSurface.get_rect()
+    rect.center = (width//2, height//2)
 
-fornt.set_italic()
-fornt.set_bold()
-fornt.set_underline()
-
-Podemos saber el tamaño de un texto con
-font.size(text) -> (width, height)
-
-Con render generamos una una superfice
-surface = font.render(texto,antialias, color)
-
-"""
+    screen.blit(textSurface, rect)
 
     pygame.display.flip()  # 
     
